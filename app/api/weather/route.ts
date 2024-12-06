b4729013d72fee25bfc15721a2a17cc3e5375b67
Jeffrey Shorthill <admin@elevatedconsultantsllc.com>
2024-12-06 09:12:46 -0700

    errors

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import axios, { AxiosError } from 'axios';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const stationId = process.env.TEMPEST_STATION_ID;
   const token = process.env.NEXT_PUBLIC_TEMPEST_TOKEN;
 
